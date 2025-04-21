3eff8ca5c60264c27d4d561ac61eca4832bf7eff
inbar007 <ibloomberg02@gmail.com>
2025-04-21 16:16:37 +0300

    update source

--- hashfunc.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-
-class hashfunc {
-public:
-
-    virtual int execute(const std::string& input) = 0; //pure virtual function
-
-    virtual ~hashfunc() = default; // virtual destructor
-};
