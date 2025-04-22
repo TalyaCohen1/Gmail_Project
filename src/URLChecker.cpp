162c651cea4ca00209ce6f9c4c1f1073c0fcf19a
inbar007 <ibloomberg02@gmail.com>
2025-04-22 15:58:57 +0300

    fix header files

---
+++ src/URLChecker.cpp
@@ -0,0 +1,27 @@
+#include "URLChecker.h"
+
+URLChecker::URLChecker(BloomFilter& bloom, URLBlacklist& blacklist) {
+    bloomFilter = bloom;
+    realBlacklist = blacklist;
+}
+
+/*
+ * Checks if the URL is blacklisted using the bloom filter and the real blacklist.
+ * Returns true if blacklisted, false otherwise.
+ * Also prints if it was a false positive.
+*/
+void URLChecker::execute(const std::string& url) {
+    // Check if the URL is in the bloom filter
+    if (bloomFilter.possiblyContain(url)) {
+        // If it is, check if it is in the real blacklist
+        std::cout << "true ";
+        if (realBlacklist.contains(url)) {
+            std::cout << "true"; // URL is blacklisted
+        } else {
+            std::cout << "False";
+        }
+    } else {
+        std::cout << "False";
+    }
+}
+
