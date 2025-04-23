c4e15ddb73b6b1da1fcebce2b54fa726f5b09c65
TalyaCohen1 <Talya.Cohen4@live.biu.ac.il>
2025-04-23 15:41:59 +0300

    add cunstactor

--- src/MultiHash.cpp
+++ src/MultiHash.cpp
@@ -3,9 +3,15 @@
 #include <string>
 
 MultiHash::MultiHash(int times) : times(times) {}
+MultiHash::MultiHash() : times(0) {} // Default constructor initializes times to 1
 
 MultiHash::~MultiHash() {}
 
+/*
+This function applies each hash function to the input string to compute
+ * multiple hash values. Each value is then mapped to an index in the bit array
+ * using modulo operation. The bits at the resulting indices are set to true.
+*/
 int MultiHash::execute(const std::string& input){
     std::string current = input;
     std::hash<std::string> myHasher;
