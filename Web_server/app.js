cb689a49059f328f2cade2666447f8676baf5f35
Inbar Bloomberg <ibloomberg02@gmail.com>
2025-05-20 14:09:14 +0300

    create files app.js and server.js

---
+++ Web_server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+// Middleware for decoding JSON in requests
+app.use(express.json());
+
+// Imports the files that define the paths for each part of the systemconst usersRoutes = require('./routes/usersRoutes');
+const mailsRoutes = require('./routes/mailsRoutes');
+const labelsRoutes = require('./routes/labelsRoutes');
+const blacklistRoutes = require('./routes/blacklistRoutes');
+const tokensRoutes = require('./routes/tokensRoutes');
+
+// Connects each group of routes to the main API
+app.use('/api/users', usersRoutes);
+app.use('/api/mails', mailsRoutes);
+app.use('/api/labels', labelsRoutes);
+app.use('/api/blacklist', blacklistRoutes);
+app.use('/api/tokens', tokensRoutes);
+
+// Handling 404
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+module.exports = app;
