c2c4faeba12f318a62df74bbe1d57bc4a7c3a6f6
TalyaCohen1 <Talya.Cohen4@live.biu.ac.il>
2025-06-01 13:00:55 +0300

    delete print messege

--- Web_server/app.js
+++ Web_server/app.js
@@ -25,8 +25,6 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is listening on port ${PORT}`);
-});
+app.listen(PORT); 
 
 module.exports = app;
