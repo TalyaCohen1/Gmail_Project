49291704cf884b1413ff8a20ffe1c8adcc9c864f
TalyaCohen1 <Talya.Cohen4@live.biu.ac.il>
2025-06-01 14:10:24 +0300

    add comments

--- Web_server/models/validator.js
+++ Web_server/models/validator.js
@@ -1,14 +1,28 @@
 
-
+/**
+ * Validates if a given string is a properly formatted date (YYYY-MM-DD)
+ * @param {*} dateStr - The date string to validate 
+ * @returns if the date string is valid
+ */
 function isValidDate(dateStr) {
   const date = new Date(dateStr);
   return !isNaN(date) && /^\d{4}-\d{2}-\d{2}$/.test(dateStr);
 }
 
+/**
+ * Validates if the gender is either "male" or "female".
+ * @param {string} gender - The gender string to validate.
+ * @returns {boolean} True if gender is valid. 
+ */
 function isValidGender(gender) {
   return gender === "male" || gender === "female";
 }
 
+/**
+ * Checks if the given email is a string and ends with "@gmail.com".
+ * @param {string} email - The email string to validate.
+ * @returns {boolean} True if the email is a valid Gmail address.
+ */
 function isValidGmail(email) {
   return typeof email === 'string' && email.endsWith('@gmail.com');
 }
--- Web_server/routes/tokenRoutes.js
+++ Web_server/routes/tokenRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const tokensController = require('../controllers/tokenController');
 
 //ger a token by usernuame+ password
+// POST /api/tokens
 router.post('/', tokensController.createToken);
 
 module.exports = router;
